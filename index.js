8ff04947feafd05c6632566869616f6d792e5e95
Sklpcc <christian_sm21@hotmail.com>
2018-03-19 04:26:29 -0500

    Temporary workaround to CORS

--- index.js
+++ index.js
@@ -5,6 +5,20 @@ import Schema from './schema';
 const port = 3000;
 const app = Express();
 
+app.use(function(req, res, next) {
+    // Temporary workaround to CORS
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Headers', 'content-type, authorization, content-length, x-requested-with, accept, origin');
+    res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+    res.header('Allow', 'POST, GET, OPTIONS');
+    res.header('Access-Control-Allow-Origin', '*');
+    if(req.method === 'OPTIONS') {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
+});
+
 app.use('/graphql', GraphHTTP({
     schema: Schema,
     pretty: true,
